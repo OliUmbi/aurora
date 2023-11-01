b81a53009d0bb48fa83fff8b7002ea32276dcf52
oliUmbi <oliver.umbricht@hispeed.ch>
2023-11-02 00:26:51 +0100

    init

---
+++ src/app.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+use yew::prelude::*;
+use yew_router::history::{AnyHistory, History, MemoryHistory};
+use yew_router::prelude::*;
+
+use crate::pages::home::Home;
+use crate::pages::not_found::NotFound;
+
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component]
+pub fn App() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+#[derive(Properties, PartialEq, Eq, Debug)]
+pub struct ServerAppProps {
+    pub url: AttrValue,
+    pub queries: HashMap<String, String>,
+}
+
+#[function_component]
+pub fn ServerApp(props: &ServerAppProps) -> Html {
+    let history = AnyHistory::from(MemoryHistory::new());
+    history
+        .push_with_query(&*props.url, &props.queries)
+        .unwrap();
+
+    html! {
+        <Router history={history}>
+            <Switch<Route> render={switch} />
+        </Router>
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => {
+            html! { <Home /> }
+        }
+        Route::NotFound => {
+            html! { <NotFound /> }
+        }
+    }
+}
---
+++ src/bin/aurora.rs
@@ -0,0 +1,5 @@
+pub use aurora::*;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
